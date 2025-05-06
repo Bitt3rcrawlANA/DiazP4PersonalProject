d3b562ec73437c24597d7ea4ed7f8e7cc2303882
Bitt3rcrawlANA <722697@student.dusd.net>
2025-05-06 11:43:42 -0700

    rt67u8kom7ik56
    567ikyr67i5ej7i5kj66ui4j5

--- PersonalDiazP4/Assets/Scripts/PlayerController.cs
+++ PersonalDiazP4/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float speed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float verticalInput = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
+        transform.Translate(Vector3.forward * verticalInput);
+        transform.Translate(Vector3.right * horizontalInput);
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        
     }
 }
